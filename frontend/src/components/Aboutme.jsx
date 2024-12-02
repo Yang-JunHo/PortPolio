ab131cbf3ab8e1a95dca2001c0835eff107811fd
Yang-JunHo <yjy3070@naver.com>
2024-12-02 15:20:06 +0900

    components animation

--- frontend/src/components/Aboutme.jsx
+++ frontend/src/components/Aboutme.jsx
@@ -3,7 +3,7 @@ import { React, useState } from 'react'
 // import '../css/AboutMe.css'
 // import useScrollAnimation from '../hooks/useScrollAnimation';
 // import ScrollAni from '../ScrollAni';
-import tw from 'tailwind-styled-components';
+// import tw from 'tailwind-styled-components';/
 import { IoLogoHtml5 } from "react-icons/io";
 import { IoLogoCss3 } from "react-icons/io";
 import { IoLogoJavascript } from "react-icons/io5";
@@ -12,22 +12,8 @@ import { IoLogoNodejs } from "react-icons/io5";
 import { IoLogoPython } from "react-icons/io5";
 import { SiMysql } from "react-icons/si";
 import { SiOracle } from "react-icons/si";
+import { motion } from "framer-motion"
 
-
-export const RecordComponent = tw.article`
-  grid
-  grid-cols-2
-  grid-row-3
-  gap-4
-  pt-[3rem]
-  mt-[3rem]
-  
-  max-md:pt-[5rem]
-  max-lg:h-auto
-  max-lg:grid-cols-1
-  max-xl:grid-cols-2  
-  max-2xl:h-screen
-`;
 
 // JavaScript에는 인터페이스가 없기 때문에 이를 삭제합니다.
 // export const TabsPropsT = {
@@ -35,9 +21,9 @@ export const RecordComponent = tw.article`
 //   id: undefined // 선택적인 속성은 undefined로 처리할 수 있습니다.
 // };
 
-const Aboutme = ({ id, navTabs }) => {
+const Aboutme = () => {
 
-//   const { scrollRef, scrollEl } = useScrollAnimation();
+  //   const { scrollRef, scrollEl } = useScrollAnimation();
 
   const [buttonText, setButtonText] = useState("자세히 보기 ➔");
 
@@ -54,54 +40,65 @@ const Aboutme = ({ id, navTabs }) => {
   };
 
   return (
-        <div className="about-container">
-          <section className="about-section">
-            <h1>About me</h1>
-            <div className="avatar">
-              {/* <img src='/img/profile.png' className="avatar-image" alt='프로필'></img> */}
-              <img src={`${process.env.PUBLIC_URL}/img/profile.png`} className="avatar-image" alt='프로필'></img>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: false }}
+      transition={{
+        ease: "easeInOut",
+        duration: 2,
+        y: { duration: 1 },
+      }}
+    >
+      <div className="about-container">
+        <section className="about-section">
+          <h1>About me</h1>
+          <div className="avatar">
+            {/* <img src='/img/profile.png' className="avatar-image" alt='프로필'></img> */}
+            <img src={`${process.env.PUBLIC_URL}/img/profile.png`} className="avatar-image" alt='프로필'></img>
+          </div>
+          <div className="hashtags">
+            <span># 유연한 사고</span>
+            <span># 업무에 대한 책임감</span>
+            <span># 오픈 마인드</span>
+            <span># 역지사지</span>
+            <span># 팀원간의 소통과 협업</span>
+          </div>
+          <button
+            className="team-button"
+            onClick={() => handleDownload()}
+            onMouseEnter={() => setButtonText("PDF 다운로드")} // 마우스 오버 시
+            onMouseLeave={() => setButtonText("자세히 보기 ➔")} // 마우스가 떠날 때
+          >
+            <span className="arrow">{buttonText}</span>
+          </button>
+          <h1>Skills</h1>
+          <div className='skillList'>
+            <div className='skillDetail'>
+              <h2>Frontend</h2>
+              {/* <img src={`${process.env.PUBLIC_URL}/img/html.png`} className="skill-image" alt="html" /> */}
+              <IoLogoHtml5 size="3em" color="red" />
+              <IoLogoCss3 size="3em" color="blue" />
+              <IoLogoJavascript size="3em" color="yellow" />
+              <IoLogoReact size="3em" color="skyblue" />
             </div>
-            <div className="hashtags">
-              <span># 유연한 사고</span>
-              <span># 업무에 대한 책임감</span>
-              <span># 오픈 마인드</span>
-              <span># 역지사지</span>
-              <span># 팀원간의 소통과 협업</span>
+            <div className='skillDetail'>
+              <h2>Backend</h2>
+              {/* <img src={`${process.env.PUBLIC_URL}/img/html.png`} className="skill-image" alt="html" /> */}
+              <IoLogoNodejs size="3em" color="green" />
+              <IoLogoPython size="3em" color="white" />
             </div>
-            <button
-              className="team-button"
-              onClick={() => handleDownload()}
-              onMouseEnter={() => setButtonText("PDF 다운로드")} // 마우스 오버 시
-              onMouseLeave={() => setButtonText("자세히 보기 ➔")} // 마우스가 떠날 때
-            >
-              <span className="arrow">{buttonText}</span>
-            </button>
-            <h1>Skills</h1>
-            <div className='skillList'>
-              <div className='skillDetail'>
-                <h2>Frontend</h2>
-                {/* <img src={`${process.env.PUBLIC_URL}/img/html.png`} className="skill-image" alt="html" /> */}
-                <IoLogoHtml5 size="3em" color="red" />
-                <IoLogoCss3 size="3em" color="blue" />
-                <IoLogoJavascript size="3em" color="yellow" />
-                <IoLogoReact size="3em" color="skyblue" />
-              </div>
-              <div className='skillDetail'>
-                <h2>Backend</h2>
-                {/* <img src={`${process.env.PUBLIC_URL}/img/html.png`} className="skill-image" alt="html" /> */}
-                <IoLogoNodejs size="3em" color="green" />
-                <IoLogoPython size="3em" color="white" />
-              </div>
-              <div className='skillDetail'>
-                <h2>DataBase</h2>
-                {/* <img src={`${process.env.PUBLIC_URL}/img/html.png`} className="skill-image" alt="html" /> */}
-                <SiMysql size="3em" color="white" />
-                <SiOracle size="3em" color="orange" />
-              </div>
+            <div className='skillDetail'>
+              <h2>DataBase</h2>
+              {/* <img src={`${process.env.PUBLIC_URL}/img/html.png`} className="skill-image" alt="html" /> */}
+              <SiMysql size="3em" color="white" />
+              <SiOracle size="3em" color="orange" />
             </div>
-          </section>
-        </div>
+          </div>
+        </section>
+      </div>
+    </motion.div>
   )
-}
+};
 
-export default Aboutme
+export default Aboutme;
--- frontend/src/components/Project.jsx
+++ frontend/src/components/Project.jsx
@@ -2,41 +2,37 @@ import '../App.css';
 import React from 'react'
 import ProjectCard from './ProjectCard'
 import projectList from '../json/project.json'
+import { motion } from 'framer-motion'
 // import useScrollAnimation from '../hooks/useScrollAnimation';
 // import ScrollAni from '../ScrollAni';
-import tw from 'tailwind-styled-components';
+// import tw from 'tailwind-styled-components';
 
-export const ProjectComponent = tw.section`
-  grid
-  grid-cols-4
-  grid-row-3
-  gap-4
-  pt-[3rem]
-  h-auto
 
-  max-md:pt-[3rem]
-  max-lg:grid-cols-1
-  max-xl:grid-cols-2  
-  max-2xl:grid-cols-3
-`;
 
-export const Container = tw.article`
-  pt-20
-`;
-
-const Project = ({ id, navTabs }) => {
-//   const { scrollRef, scrollEl } = useScrollAnimation();
+const Project = () => {
+  //   const { scrollRef, scrollEl } = useScrollAnimation();
   let project = projectList.projectList.map(item => <ProjectCard key={item.title} data={item} id={item.Mtitle}></ProjectCard>)
 
   return (
-        <div className='project-container'>
-          <section className='project-section'>
-            <h1>Project</h1>
-            <div className='card-container'>
-              {project}
-            </div>
-          </section>
-        </div>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: false }}
+      transition={{
+        ease: "easeInOut",
+        duration: 2,
+        y: { duration: 1 },
+      }}
+    >
+      <div className='project-container'>
+        <section className='project-section'>
+          <h1>Project</h1>
+          <div className='card-container'>
+            {project}
+          </div>
+        </section>
+      </div>
+    </motion.div>
   )
 }
 
